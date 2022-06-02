4b242590632aa943b5e6c5836a2ab4d428b799a9
imnrb <67147637+imnrb@users.noreply.github.com>
2022-06-02 18:47:20 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const app = express();
+const router=require("./routes/book-routes");
+//middleware
+app.use(express.json());
+app.use("/books",router)
+
+mongoose.connect("mongodb+srv://admin:oubUOQOe2x3LsRNI@cluster0.o2yoz3e.mongodb.net/bookStore?retryWrites=true&w=majority")
+.then(() => console.log("connected to database"))
+.then(() => {app.listen(5000)}).catch((err)=>console.log(err));
+
+
+
+
+//oubUOQOe2x3LsRNI
